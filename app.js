c48392bc962fd98d6952d44a14606f18148dcdf3
abhay-rana <abhayrana286@gmail.com>
2023-05-03 13:20:43 +0530

    react native svg transformer

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 
-import CompOne from './src/components/comp-one';
+import CompTwo from '~/components/comp-two';
 
 const App = () => {
     const [state, setState] = useState();
     return (
         <View className="flex-1">
             <Text className="bg-blue-400 text-red-300">Abhay rana</Text>
-            <CompOne className="h-4 w-4 p-4" />
+            <CompTwo />
         </View>
     );
 };
---
+++ src/components/comp-two.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CompTwo = () => {
+    return (
+        <View>
+            <Text>CompTwo</Text>
+        </View>
+    );
+};
+
+export default CompTwo;
