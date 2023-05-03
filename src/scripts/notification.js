cfd8e8e630ab103eb6c56f12f1ccaf5c5aa10b64
abhay-rana <abhayrana286@gmail.com>
2023-05-03 16:00:43 +0530

    path alias and boolean

--- src/scripts/notification.js
+++ src/scripts/notification.js
@@ -1,60 +1,92 @@
+import notifee, { AndroidImportance } from '@notifee/react-native';
 import messaging from '@react-native-firebase/messaging';
-import {Alert} from 'react-native';
-
-// this is for the priority setup
-// {
-//     "android": {
-//         "priority": "high" // "high" , "low" , "normal"
-//     }
-// }
+import { Alert } from 'react-native';
 
 /**
  * @description Get the device token if permission is granted
  */
-export const GetFCMToken = async () => {
-  try {
-    const fcm_token = await messaging().getToken();
 
-    if (fcm_token) {
-      console.log('** FCM Token **', fcm_token);
-      return fcm_token;
+// Request Permission
+const _requestPermission = async () => {
+    try {
+        const permissions = await messaging().requestPermission();
+        console.log('permission', permissions);
+        if (!!permissions) await GetFCMToken();
+        return permissions;
+    } catch (error) {
+        Alert.alert('permission rejected');
+        console.error('Permissions', error);
     }
-  } catch (err) {
-    console.error('Get FCM Token:', err);
-  }
 };
 
-export const InitializeNotification = () => {
-  GetFCMToken();
-  // if the app is the background state -> clicked/tapped
-  messaging().onNotificationOpenedApp(remoteMessage => {
-    console.log(
-      'Notification caused app to open from background state:',
-      remoteMessage.notification,
-    );
-  });
+export const GetFCMToken = async () => {
+    try {
+        const fcm_token = await messaging().getToken();
 
-  // Quiet and Background State -> Check whether an initial notification is available ->clicked/tapped
-  messaging()
-    .getInitialNotification()
-    .then(remoteMessage => {
-      if (remoteMessage) {
-        console.log(
-          'Notification caused app to open from quit state:',
-          remoteMessage.notification,
-        );
-      }
-    })
-    .catch(error => console.log('failed', error));
+        if (fcm_token) {
+            console.log('** FCM Token **', fcm_token);
+            return fcm_token;
+        }
+    } catch (err) {
+        console.error('Get FCM Token:', err);
+    }
+};
 
-  // Foreground State if the push notification is trigger when the app is open
-  messaging().onMessage(async remoteMessage => {
-    console.log('foreground', remoteMessage);
-  });
+export const InitializeNotification = async () => {
+    const permissions = await _requestPermission();
+    if (!!permissions) {
+        // if the app is the background state -> clicked/tapped
+        messaging().onNotificationOpenedApp((remoteMessage) => {
+            console.log(
+                'Notification caused app to open from background state:',
+                remoteMessage.notification
+            );
+        });
 
-  // Register background handler
-  // this create the service and register the app
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-  });
+        // Quiet and Background State -> Check whether an initial notification is available ->clicked/tapped
+        messaging()
+            .getInitialNotification()
+            .then((remoteMessage) => {
+                if (remoteMessage) {
+                    console.log(
+                        'Notification caused app to open from quit state:',
+                        remoteMessage.notification
+                    );
+                }
+            })
+            .catch((error) => console.log('failed', error));
+
+        // Foreground State if the push notification is trigger when the app is open
+        messaging().onMessage(async (remoteMessage) => {
+            console.log('foreground', remoteMessage);
+            await notifee.createChannel({
+                id: 'important-reminders',
+                name: 'Important Reminders',
+                vibration: true,
+                importance: AndroidImportance.HIGH,
+            });
+            const notification = {
+                title: '',
+                body: '',
+                android: {
+                    channelId: 'important-reminders',
+                },
+            };
+            notification.body = remoteMessage.notification.body;
+            notification.title = remoteMessage.notification.title;
+            // if image is present
+            if (remoteMessage.notification?.android?.imageUrl) {
+                notification.android.largeIcon =
+                    remoteMessage.notification.android?.imageUrl;
+            }
+
+            await notifee.displayNotification(notification);
+        });
+
+        // Register background handler
+        // this create the service and register the app
+        messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+            console.log('Message handled in the background!', remoteMessage);
+        });
+    }
 };
