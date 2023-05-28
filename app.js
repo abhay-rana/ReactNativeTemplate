1af45816c449e9ed7c38edd023361a45a2dc4b84
abhay-rana <abhayrana286@gmail.com>
2023-05-28 13:45:30 +0530

    text component

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import 'react-native-gesture-handler';
 
 import tw from '~/styles/tailwind';
@@ -7,29 +7,24 @@ import tw from '~/styles/tailwind';
 import ArrowNext from '~/assets/svg/arrow-next.svg';
 
 import CompTwo from '~/components/comp-two';
-
-import { POPPINS_BOLD } from '~/scripts/theme-var';
+import Text from '~/components/library/text';
 
 const App = () => {
     const [state, setState] = useState();
     return (
         <View style={tw`flex-1`}>
-            <Text style={tw` text-3xl font-poppins`}>this is normal text</Text>
-            <Text style={tw` text-3xl font-kalam font-bold`}>
-                this is normal text
+            <Text
+                as="h3"
+                font_type="secondary"
+                font_weight={'w300'}
+                color="black"
+            >
+                hello this is abhay
             </Text>
-            <Text style={[styles.kalam, tw``]}>this is normal text</Text>
-            <CompTwo />
-            <ArrowNext width={10} height={20} className="text-red-600" />
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    kalam: {
-        fontFamily: POPPINS_BOLD,
-        fontSize: 30,
-    },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ src/components/library/text.js
@@ -0,0 +1,132 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Text as RNText, View } from 'react-native';
+
+import tw from '~/styles/tailwind';
+
+import {
+    PRIMARY_FONT_BLACK,
+    PRIMARY_FONT_BOLD,
+    PRIMARY_FONT_SEMIBOLD,
+    SECONDARY_FONT_BLACK,
+    SECONDARY_FONT_LIGHT,
+    SECONDARY_FONT_REGULAR,
+} from '~/scripts/theme-var';
+
+const primary_weight = {
+    w300: {
+        fontFamily: PRIMARY_FONT_SEMIBOLD,
+    },
+    w500: {
+        fontFamily: PRIMARY_FONT_BOLD,
+    },
+    w700: {
+        fontFamily: PRIMARY_FONT_BLACK,
+    },
+};
+const secondary_weight = {
+    w300: {
+        fontFamily: SECONDARY_FONT_LIGHT,
+    },
+    w500: {
+        fontFamily: SECONDARY_FONT_REGULAR,
+    },
+    w700: {
+        fontFamily: SECONDARY_FONT_BLACK,
+    },
+};
+
+const getStyle = {
+    h1: {
+        fontFamily: PRIMARY_FONT_BLACK,
+        fontSize: 23,
+    },
+    h2: {
+        fontFamily: PRIMARY_FONT_BOLD,
+        fontSize: 20,
+    },
+    h3: {
+        fontFamily: PRIMARY_FONT_SEMIBOLD,
+        fontSize: 18,
+    },
+    p: {
+        fontFamily: PRIMARY_FONT_SEMIBOLD,
+        fontSize: 16,
+    },
+    p2: {
+        fontFamily: SECONDARY_FONT_BLACK,
+        fontSize: 16,
+    },
+};
+const getTextStyle = (type, weight) => {
+    if (type === 'primary' && !weight) {
+        return {
+            fontFamily: PRIMARY_FONT_SEMIBOLD,
+        };
+    } else if (type === 'primary' && weight) {
+        return primary_weight[weight];
+    } else if (type === 'secondary' && !weight) {
+        return {
+            fontFamily: SECONDARY_FONT_LIGHT,
+        };
+    } else if (type === 'secondary' && weight) {
+        return secondary_weight[weight];
+    }
+};
+
+const Text = ({
+    style,
+    children,
+    as = 'p',
+    font_type = 'primary',
+    font_weight,
+    color,
+    ...props
+}) => {
+    let extra_class;
+    extra_class = getStyle[as];
+    const classes = getTextStyle(font_type, font_weight);
+    console.log({ extra_class, classes, style });
+    let color_class;
+    if (color === 'primary') {
+        color_class += ` text-primary`;
+    } else if (color === 'secondary') {
+        color_class += ` text-secondary`;
+    } else if (color === 'white') {
+        color_class += ` text-white`;
+    } else if (color === 'black') {
+        color_class += ` text-black`;
+    } else if (color === 'danger') {
+        color_class += ` text-danger`;
+    } else if (color === 'success') {
+        color_class += ` text-success`;
+    } else if (color === 'warning') {
+        color_class += ` text-warning`;
+    }
+    return (
+        <View>
+            <RNText style={[extra_class, classes, style, tw`${color_class}`]}>
+                {children}
+            </RNText>
+        </View>
+    );
+};
+
+Text.propTypes = {
+    children: PropTypes.node.isRequired,
+    style: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+    as: PropTypes.oneOf(['h1', 'h2', 'p']),
+    font_type: PropTypes.oneOf(['primary', 'secondary']),
+    font_weight: PropTypes.oneOf(['w100', 'w300', 'w700', 'w900']),
+    color: PropTypes.oneOf([
+        'primary',
+        'secondary',
+        'danger',
+        'success',
+        'black',
+        'warning',
+        'white',
+    ]),
+};
+
+export default Text;
