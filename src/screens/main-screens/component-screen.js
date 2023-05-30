8a48dfad453caa935f7081101ca6c0bfefcd2d7b
abhay-rana <abhayrana286@gmail.com>
2023-05-30 22:30:11 +0530

    text changes

--- src/screens/main-screens/component-screen.js
+++ src/screens/main-screens/component-screen.js
@@ -6,6 +6,7 @@ import { CHECKBOX_SCREEN, TEXT_SCREEN } from '~/navigation/navigation-constant';
 import tw from '~/styles/tailwind';
 
 import Button from '~/components/library/button';
+import Text from '~/components/library/text';
 
 const ComponentScreen = () => {
     const navigation = useNavigation();
@@ -23,6 +24,12 @@ const ComponentScreen = () => {
             >
                 TestScreen
             </Button>
+            <Text style={tw`text-10`}>Hello this is one</Text>
+            <Text style={tw`text-11`}>Hello this is one</Text>
+            <Text style={tw`text-12`}>Hello this is one</Text>
+            <Text style={tw`text-16`}>Hello this is one</Text>
+            <Text style={tw`text-lg`}>Hello this is one</Text>
+            <Text style={tw`text-24`}>Hello this is one</Text>
         </View>
     );
 };
