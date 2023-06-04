39d5b0227dc520fc2f35cad516102c52e0c2c407
abhay-rana <abhayrana286@gmail.com>
2023-06-04 19:27:07 +0530

    useLocation hook

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,3 +1,26 @@
-import { createNavigationContainerRef } from '@react-navigation/native';
+import {
+    createNavigationContainerRef,
+    useFocusEffect,
+} from '@react-navigation/native';
+import { useCallback, useState } from 'react';
 
 export const navigationRef = createNavigationContainerRef();
+
+export const useLocation = () => {
+    const [location, setLocation] = useState();
+    useFocusEffect(
+        useCallback(() => {
+            if (navigationRef.isReady()) {
+                const screen_name = navigationRef.getCurrentRoute().name;
+                setLocation(screen_name);
+            } else {
+                setTimeout(() => {
+                    const screen_name = navigationRef.getCurrentRoute().name;
+                    setLocation(screen_name);
+                }, 0);
+            }
+        }, [])
+    );
+
+    return [location, setLocation];
+};
